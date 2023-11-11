1bfd3842b17b55bd8880f9c52d16187d814222c6
Damien Nelson <dhnisaman@gmail.com>
2023-11-11 10:35:59 -0500

    testing switch statements in c#

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -3,22 +3,37 @@
 // string flip = condition.Next(1,3) == 1 ? "heads" : "tails";
 // Console.WriteLine($"The coins face is {flip}");
 
-string permission = "Admin|Manager";
-int level = 55;
+// string permission = "Admin|Manager";
+// int level = 55;
 
-bool admin = permission.Contains("Admin");
-bool adminLevel = level > 55? true : false;
+// bool admin = permission.Contains("Admin");
+// bool adminLevel = level > 55? true : false;
 
-bool manager = permission.Contains("Manager");
-bool managerLevel = level >= 20 ? true : false;
+// bool manager = permission.Contains("Manager");
+// bool managerLevel = level >= 20 ? true : false;
 
 
-if(admin && adminLevel){
-    Console.WriteLine("Welcome Super Admin");
-} else if(admin && !adminLevel){
-    Console.WriteLine("Welcome, Admin user.");
-} else if(manager && managerLevel){
-    Console.WriteLine("Contact an Admin for access.");
-} else {
-    Console.WriteLine("You do not have sufficient privileges.");
+// if(admin && adminLevel){
+//     Console.WriteLine("Welcome Super Admin");
+// } else if(admin && !adminLevel){
+//     Console.WriteLine("Welcome, Admin user.");
+// } else if(manager && managerLevel){
+//     Console.WriteLine("Contact an Admin for access.");
+// } else {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
+
+// Switch statement
+string fruit = "apple";
+
+switch(fruit){
+    case "apple":
+    Console.WriteLine(fruit);
+    break;
+    case "banana":
+    Console.WriteLine(fruit);
+    break;
+    default:
+    Console.WriteLine("not a valid fruit");
+    break;
 }
