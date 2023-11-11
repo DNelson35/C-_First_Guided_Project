89c97957c3e46e468dd6dd3a28b04b0b7339984b
Damien Nelson <dhnisaman@gmail.com>
2023-11-11 10:03:10 -0500

    code challenge my solution

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -1,4 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random condition = new Random();
-string flip = condition.Next(1,3) == 1 ? "heads" : "tails";
-Console.WriteLine($"The coins face is {flip}");
+// Random condition = new Random();
+// string flip = condition.Next(1,3) == 1 ? "heads" : "tails";
+// Console.WriteLine($"The coins face is {flip}");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+bool admin = permission.Contains("Admin");
+bool adminLevel = level > 55? true : false;
+
+bool manager = permission.Contains("Manager");
+bool managerLevel = level >= 20 ? true : false;
+
+
+if(admin && adminLevel){
+    Console.WriteLine("Welcome Super Admin");
+} else if(admin && !adminLevel){
+    Console.WriteLine("Welcome, Admin user.");
+} else if(manager && managerLevel){
+    Console.WriteLine("Contact an Admin for access.");
+} else {
+    Console.WriteLine("You do not have sufficient privileges.");
+}
