3a64a9a74c7f34c5af96166d72794c4bb063c93b
Damien Nelson <dhnisaman@gmail.com>
2023-11-11 09:36:19 -0500

    implement ternary  operation

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("a" == "b");
-Console.WriteLine("c" == "c");
-Console.WriteLine(1 == 3);
+Random condition = new Random();
+string flip = condition.Next(1,3) == 1 ? "heads" : "tails";
+Console.WriteLine($"The coins face is {flip}");
