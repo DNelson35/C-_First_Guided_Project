bdbfb570540817e5385023bbf3495f360d92e9a7
Damien Nelson <dhnisaman@gmail.com>
2023-11-12 19:52:39 -0500

    error handeling

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -237,36 +237,74 @@
 
 // guess game
 
-using System.Runtime.CompilerServices;
+// using System.Runtime.CompilerServices;
 
-Random rand = new Random();
-int currentGuess;
-bool playing = true;
-string? userSelected;
+// Random rand = new Random();
+// int currentGuess;
+// bool playing = true;
+// string? userSelected;
 
-Console.WriteLine("Do you want to play a guessing game? (y/n): ");
-userSelected = Console.ReadLine();
+// Console.WriteLine("Do you want to play a guessing game? (y/n): ");
+// userSelected = Console.ReadLine();
 
-if(userSelected != null && userSelected == "y"){
-    Console.WriteLine("lets Play");
-    int computerPick = rand.Next(1, 10);
-    while(playing == true){
-        Console.WriteLine("Pick a number");
-        string? userPick;
-        userPick = Console.ReadLine();
-        if(int.TryParse(userPick, out currentGuess)){
-            userGuess(userPick, computerPick);
-        }
-    }   
-}else{
-    Console.WriteLine("Ok goodbye");
-}
+// if(userSelected != null && userSelected == "y"){
+//     Console.WriteLine("lets Play");
+//     int computerPick = rand.Next(1, 10);
+//     while(playing == true){
+//         Console.WriteLine("Pick a number");
+//         string? userPick;
+//         userPick = Console.ReadLine();
+//         if(int.TryParse(userPick, out currentGuess)){
+//             userGuess(userPick, computerPick);
+//         }
+//     }   
+// }else{
+//     Console.WriteLine("Ok goodbye");
+// }
 
-void userGuess(string userPick, int computerPick){
-    if (currentGuess == computerPick){
-        Console.WriteLine($"You Win computer picked {computerPick}");
-        playing = false;
-    } else {
-        Console.WriteLine($"Sorry try again {userPick} is the wrong number");
-    }
+// void userGuess(string userPick, int computerPick){
+//     if (currentGuess == computerPick){
+//         Console.WriteLine($"You Win computer picked {computerPick}");
+//         playing = false;
+//     } else {
+//         Console.WriteLine($"Sorry try again {userPick} is the wrong number");
+//     }
+// }
+
+// using System.Globalization;
+
+// int[] schedule = {800, 1200, 1600, 2000};
+
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
+// {
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//     {
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     } 
+//     else 
+//     {
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     }
+
+//     for (int i = 0; i < times.Length; i++) 
+//     {
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
+//     }
+// }
+
+// catching exceptions
+int errorVal = 0;
+
+try 
+{
+    decimal number = 3m / errorVal;
+} 
+catch {
+    Console.WriteLine("Error:");
 }
