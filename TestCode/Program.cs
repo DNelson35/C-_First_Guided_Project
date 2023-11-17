310988be3ba48bd6da6f87dab05d66d36e3fcf84
Damien Nelson <dhnisaman@gmail.com>
2023-11-16 22:48:10 -0500

    trying leetcode c#

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -309,11 +309,36 @@
 //     Console.WriteLine("Error:");
 // }
 
-using System.Xml.Schema;
+// using System.Xml.Schema;
 
-int displayNumber(int a, int b){
-     int total = a + b;
-    return total;
-}
+// int displayNumber(int a, int b){
+//      int total = a + b;
+//     return total;
+// }
 
-Console.WriteLine(displayNumber(1, 5));
+// Console.WriteLine(displayNumber(1, 5));
+
+// let Code merging two sorted arrays
+// public class Solution {
+//     public void Merge(int[] nums1, int m, int[] nums2, int n) {
+//           int i = m - 1;
+//    int j = n - 1;
+//    int k = m + n - 1;
+
+//    while (i >= 0 && j >= 0)
+//    {
+//        if (nums1[i] > nums2[j])
+//        {
+//            nums1[k--] = nums1[i--];
+//        }
+//        else
+//        {
+//            nums1[k--] = nums2[j--];
+//        }
+//    }
+//    while (j >= 0)
+//    {
+//        nums1[k--] = nums2[j--];
+//    }
+//     }
+// }
